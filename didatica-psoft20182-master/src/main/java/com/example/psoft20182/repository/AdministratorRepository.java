7c23847b1556caeb3da9f28a16e20391d231e82b
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2018-11-29 15:34:45 -0300

    adm repo

--- didatica-psoft20182-master/src/main/java/com/example/psoft20182/repository/AdministratorRepository.java
+++ didatica-psoft20182-master/src/main/java/com/example/psoft20182/repository/AdministratorRepository.java
@@ -1,9 +1,20 @@
 package com.example.psoft20182.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import com.example.psoft20182.model.produto.Product;
+import com.example.psoft20182.model.usuarios.Administrator;
 
 @Repository
 
-public interface AdministratorRepository {
+public interface AdministratorRepository extends JpaRepository<Administrator,String> {
+
+	@Query("select u from Administrator u where u.name = ?1")
+	public Administrator findAdmByName(String name);
+
+	@Query("select u from Administrator u where u.email = ?1")
+	public Administrator findAdmByEmail(String name);
 
 }
